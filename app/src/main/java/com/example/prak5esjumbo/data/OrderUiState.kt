df389284d50416d6a4acae348a9f918fd5cf191a
Akbaradji <akbaradjiherlambang@gmail.com>
2023-11-09 18:19:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prak5esjumbo/data/OrderUiState.kt
@@ -0,0 +1,7 @@
+package com.example.prak5esjumbo.data
+
+data class OrderUiState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+)
---
+++ app/src/main/java/com/example/prak5esjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.prak5esjumbo.data
+
+import com.example.prak5esjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
